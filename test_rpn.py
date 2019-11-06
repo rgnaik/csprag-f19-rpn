ed2794ce2abd4ce944e1be5f9dfbcf84687570a7
rgnaik <rgnaik@umich.edu>
2019-11-06 01:08:37 -0500

    mult and div

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,12 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+    def test_mult(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
+    def test_div(self):
+        result = rpn.calculate("15 3 /")
+        self.assertEqual(5, result)
     def test_badInput(self):
         with self. assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
